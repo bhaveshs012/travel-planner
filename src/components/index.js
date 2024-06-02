ad7a23f0b9e9d0d4a8b875f2b25f9ed8587e0768
Vishesh Karan <vishesh.k@somaiya.edu>
2024-06-02 07:24:03 +0530

    Fixed file importing issue

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import MainCard from "./TripCardMain/MainCard";
 import Card from "./Cards/Card";
-
 export { Header, Footer, MainCard, Card};
